f2d004a634b5d8163d98b20940794042fe5dedad
Pete Hayes <pete@hayes.id.au>
2021-08-14 09:42:55 +1000

    Remove ceiling on payment duration now that Decimal handles very small numbers

--- src/transaction.rs
+++ src/transaction.rs
@@ -7,11 +7,6 @@ use chrono::{Date, Utc};
 use rust_decimal::Decimal;
 use thiserror::Error;
 
-// This represents the maximum number of days that a time span can be.
-// NOTE! If you change this const, make sure you also update the `ExcessiveDateRange`
-// error message.
-const MAX_DAYS: u32 = (325.25 * 10.0) as u32; // 10 years
-
 /// The model for a future transaction or group of transactions.
 ///
 /// `Transaction`s are the building blocks of a budget, and are used to track
@@ -35,8 +30,6 @@ pub enum TransactionError {
     Contribution(#[from] ContributionError),
     #[error("currency values cannot have more than 2 decimal places: {0}")]
     CurrencyPrecision(Decimal),
-    #[error("date ranges greater than 10 years are unsupported")]
-    ExcessiveDateRange,
 }
 
 impl Transaction {
@@ -47,14 +40,6 @@ impl Transaction {
         start_date: Date<Utc>,
         end_date: Option<Date<Utc>>,
     ) -> Result<Self, TransactionError> {
-        // Ensure we don't have any date ranges > 10 years
-        if let Some(end) = end_date {
-            let duration = end - start_date;
-            if duration.num_days() > MAX_DAYS as i64 {
-                return Err(TransactionError::ExcessiveDateRange);
-            }
-        }
-
         let v = match value {
             TransactionValue::Fixed(v) => v,
             TransactionValue::Variable(_, v) => v,
@@ -82,20 +67,8 @@ impl Transaction {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use chrono::{Datelike, TimeZone};
+    use chrono::Datelike;
     use rust_decimal_macros::dec;
-
-    #[test]
-    fn new_transaction_excessive_date_range() {
-        let result = Transaction::new(
-            TransactionMatcher::default(),
-            TransactionValue::Fixed(dec!(0.0)),
-            Frequency::Once,
-            Utc.ymd(2000, 1, 1),
-            Some(Utc.ymd(3000, 1, 2)),
-        );
-        assert_eq!(result.err(), Some(TransactionError::ExcessiveDateRange));
-    }
 
     #[test]
     fn new_transaction_ok() {
